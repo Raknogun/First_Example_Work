2b6f3ab3b7d0de78b6a68c0669cba9b4e2b4d890
Raknogun <bon.max@seznam.cz>
2024-07-10 17:52:37 +0200

    New files

---
+++ conftest.py
@@ -0,0 +1,24 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+
+
+
+def pytest_addoption(parser):
+    parser.addoption('--language', action='store', default='en',
+                     help="Choose language")
+
+    
+
+
+@pytest.fixture(scope="function")
+def browser(request):
+    language_name = request.config.getoption("language")
+    language = None
+    options = Options()
+    options.add_experimental_option('prefs', {'intl.accept_languages': language_name})
+    browser = webdriver.Chrome(options=options)
+    yield browser
+    print("\nquit browser..")
+    browser.quit()
